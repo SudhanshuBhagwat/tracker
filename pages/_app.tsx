e9676a9373b00fd3cbae44e0503ea3e658afd96d
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-04-01 20:26:33 +0530

    Initial Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <title>Next.js PWA Example</title>
+        <meta name="theme-color" content="#319197" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <main className="h-screen w-screen">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
