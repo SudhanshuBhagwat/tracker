37d248394a68d6cdfe144d4b3ecdd63fae935540
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-04-12 21:30:40 +0530

    * Added code for total goals and expenses

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,7 +1,9 @@
+import { format } from "date-fns";
 import { getDocs, query, collection, where } from "firebase/firestore";
 import { useRouter } from "next/router";
 import React from "react";
 import useSWR from "swr";
+import Spinner from "../components/Spinner";
 import { auth, firestore, useAuth } from "../config/firebase";
 
 const goalsfetcher = async (url: string, id: string | undefined) => {
@@ -22,20 +24,58 @@ const goalsfetcher = async (url: string, id: string | undefined) => {
   return totalGoalsCompleted;
 };
 
+const expensesFetcher = async (url: string, id: string | undefined) => {
+  let totalExpenses = 0;
+
+  try {
+    const snapshots = await getDocs(
+      query(collection(firestore, url), where("createdBy", "==", id))
+    );
+
+    snapshots.forEach((snapshot) => {
+      const data = snapshot.data();
+      totalExpenses += Number(data.spent);
+    });
+  } catch (err) {
+    throw err;
+  }
+  return totalExpenses;
+};
+
 interface Props {}
 
 const Settings: React.FC<Props> = () => {
   const { currentUser } = useAuth();
-  const { data: goalsCompleted } = useSWR(
-    currentUser ? "/habits" : null,
-    (url) => goalsfetcher(url, currentUser?.uid)
-  );
+  // const { data, error } = useSWR(currentUser ? "/expenses" : null, (url) =>
+  //   goalsfetcher(url, currentUser?.uid)
+  // );
+  // const { data, error } = useSWR(currentUser ? "/habits" : null, (url) =>
+  //   goalsfetcher(url, currentUser?.uid)
+  // );
   const router = useRouter();
 
   function handleSignout() {
     router.replace("/auth");
     auth.signOut();
   }
+
+  // if (error) {
+  //   return (
+  //     <div className="h-full flex justify-center items-center">
+  //       <span className="px-4 py-2 bg-red-500 rounded-md font-medium text-white">
+  //         {error}
+  //       </span>
+  //     </div>
+  //   );
+  // }
+
+  // if (!goalsCompleted) {
+  //   return (
+  //     <div className="h-full flex justify-center items-center">
+  //       <Spinner />
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="h-full p-4">
@@ -53,11 +93,14 @@ const Settings: React.FC<Props> = () => {
             </div>
             <div className="flex justify-between">
               <label className="font-medium">Total completed Goals</label>
-              <span>{goalsCompleted}</span>
+              <span>{0}</span>
             </div>
             <div className="flex justify-between">
               <label className="font-medium">
-                Total Expenses <span className="text-gray-400">(April)</span>
+                Total Expenses{" "}
+                <span className="text-gray-400">
+                  ({format(new Date(), "LLLL")})
+                </span>
               </label>
               <span>30,000 ₹</span>
             </div>
