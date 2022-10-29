cfb8699c11f5c2ad1910fbd47e8da258b659ff0b
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-10-29 23:40:20 +0530

    Fixed Goal screen layout for both desktop and mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,11 +16,12 @@ import {
   where,
 } from "firebase/firestore";
 import { firestore, useAuth } from "../config/firebase";
-import { format } from "date-fns";
+import { format, parseISO, startOfToday } from "date-fns";
 import useSWR, { mutate as gMutate } from "swr";
 import Spinner from "../components/Spinner";
 import { useRouter } from "next/router";
 import { AnimatePresence } from "framer-motion";
+import Calendar from "../components/Calendar";
 
 const fetcher = async (
   url: string,
@@ -76,6 +77,8 @@ const Home: NextPage = () => {
   const [mode, setMode] = useState<"ADD" | "EDIT" | null>(null);
   const [selectedGoal, setSelectedGoal] = useState<GoalType | null>();
   const { addGoal, updateGoal, removeGoal } = useFirestore();
+  const today = startOfToday();
+  const [selectedDay, setSelectedDay] = useState(today);
 
   useEffect(() => {
     if (!fetchingUser && !currentUser) {
@@ -135,6 +138,106 @@ const Home: NextPage = () => {
       </div>
     );
   }
+
+  return (
+    <div className="h-full flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-7">
+      <section className="lg:col-span-5 p-4 sm:col-span-1 md:col-span-1 lg:p-0">
+        <div className="w-full space-y-4 lg:space-y-0 lg:mt-0 lg:grid lg:grid-cols-2 lg:h-full">
+          <div className="flex flex-col lg:border-r lg:p-4">
+            <div className="flex justify-between items-center lg:items-start">
+              <h2 className="text-2xl font-semibold">Goals</h2>
+              <button
+                className="flex items-center bg-blue-200 px-2 py-1 rounded-md"
+                onClick={() => {
+                  setMode("ADD");
+                  setSelectedGoal(null);
+                  setIsOpen((open) => !open);
+                }}
+              >
+                <span className="mr-1">
+                  <PlusIcon className="h-4 w-4" />
+                </span>
+                Add Goal
+              </button>
+            </div>
+            <h3 className="text-xl font-semibold mt-2 lg:mt-4">Today</h3>
+            {data.todaysGoals.length > 0 ? (
+              <div className="mt-2 space-y-2">
+                {data.todaysGoals.map((goal) => {
+                  return (
+                    <Goal
+                      key={goal.id}
+                      goal={goal}
+                      disable={false}
+                      handleDone={handleDone}
+                      handleClick={() => {
+                        setSelectedGoal(goal);
+                        setMode("EDIT");
+                        setIsOpen((open) => !open);
+                      }}
+                    />
+                  );
+                })}
+              </div>
+            ) : (
+              <div className="h-full flex flex-col justify-center items-center space-y-2 py-6">
+                <span>You&apos;re good for today</span>
+              </div>
+            )}
+          </div>
+          <div className="lg:p-4 lg:h-full">
+            <h3 className="text-xl font-semibold">All Goals</h3>
+            {data.collectionGoals.length > 0 ? (
+              <div className="mt-2 space-y-2">
+                {data.collectionGoals.map((goal) => {
+                  return (
+                    <Goal
+                      key={goal.id}
+                      goal={goal}
+                      handleDone={handleDone}
+                      disable
+                    />
+                  );
+                })}
+              </div>
+            ) : (
+              <div className="flex flex-col justify-center items-center space-y-2 mt-20 lg:h-full lg:mt-0">
+                <span>No Goals added</span>
+                <span className="flex items-center">
+                  Press
+                  <button className="flex items-center bg-blue-200 px-2 py-1 rounded-md mx-2">
+                    <span className="mr-1">
+                      <PlusIcon className="h-4 w-4" />
+                    </span>
+                    Add Goal
+                  </button>
+                  to add one
+                </span>
+              </div>
+            )}
+          </div>
+        </div>
+        <AnimatePresence>
+          {isOpen && (
+            <AddGoal
+              setIsOpen={setIsOpen}
+              handleSubmit={handleSubmit}
+              handleRemove={handleRemove}
+              mode={mode}
+              goal={selectedGoal}
+            />
+          )}
+        </AnimatePresence>
+      </section>
+      <section className="lg:col-span-2 border-l hidden sm:col-span-1 sm:block">
+        <Calendar
+          mode="monthly"
+          selectedDay={selectedDay}
+          setSelectedDay={setSelectedDay}
+        />
+      </section>
+    </div>
+  );
 
   return (
     <div className="h-full p-4">
